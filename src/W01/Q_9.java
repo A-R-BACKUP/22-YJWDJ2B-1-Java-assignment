1ba3dbbf44caf94a28f5010fb227fc51fe8ae628
ASTER18K <donhon05@gmail.com>
2022-03-13 22:47:41 +0900

    20220313-1
    public_branch

---
+++ src/W01/Q_9.java
@@ -0,0 +1,35 @@
+package W01;
+
+import java.util.Scanner;
+
+public class Q_9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("+, -, *, / 중 원하는 작업 입력");
+        String operation = sc.next();
+        if (operation == "+" || operation == "-" || operation == "*" || operation == "/") {
+            System.out.printf("첫번째 숫자 입력");
+            int a = sc.nextInt();
+            System.out.printf("두번째 숫자 입력");
+            int b = sc.nextInt();
+            switch (operation){
+                case "+" :
+                    System.out.printf(a + " + " + b + " = " + a + b);
+                    break;
+                case "-" :
+                    System.out.printf(a + " - " + b + " = " + (a - b));
+                    break;
+                case "*" :
+                    System.out.printf(a + " + " + b + " = " + a + b);
+                    break;
+                case "/" :
+                    System.out.printf(a + " + " + b + " = " + a + b);
+                    break;
+            }
+
+        } else {
+            System.out.printf("잘못 입력하였습니다.");
+        }
+
+    }
+}
