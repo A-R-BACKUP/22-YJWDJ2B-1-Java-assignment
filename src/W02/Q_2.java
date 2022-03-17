cfde6f1b0a7e7ab434040d795ba5521f07f4998d
ASTER18K <donhon05@gmail.com>
2022-03-17 20:50:29 +0900

    20220317 - 2

--- src/W02/Q_2.java
+++ src/W02/Q_2.java
@@ -4,11 +4,12 @@ package W02;
 public class Q_2 {
     public static void main(String[] args) {
         for (int i = 2; i <= 100; i++) {
-            for (int j = 2; j == (i - 1); j++)
-            if ((i & j) == 0){
-            } else {
-                System.out.printf(" %d", i);
+            for (int j = 2; j == (i - 1); j++){
+                if ((i % j) == 0){
+                    continue;
+                }
             }
+            System.out.printf(i+" ");
         }
     }
 }
