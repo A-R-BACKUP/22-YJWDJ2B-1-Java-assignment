caff052695157998b6f65f7af4ce04cf7cf21034
ASTER18K <donhon05@gmail.com>
2022-03-29 13:48:13 +0900

    20220328

--- src/W03/Q_2_With_explation.java
+++ src/W03/Q_2_With_explation.java
@@ -1,8 +1,18 @@
 package W03;
 
-import java.util.ArrayList;
+/*
+101의 제네레이터
+91, 100
 
-public class Q_2 {
+9 + 1 + 91 = 101
+1 + 0 + 0 + 100 = 101
+
+이런식인데...
+ */
+
+import java.util.*;
+
+public class Q_2_With_explation {
     public static void main(String[] args) {
         int n = 0;
         int sum = 0;
@@ -35,12 +45,17 @@ public class Q_2 {
             if (numbers.indexOf(sum) != -1) { // 어레이에 sum이 있으면
                 numbers.remove(Integer.valueOf(sum)); // 어레이에 129가 있으면 제거
             }
+            // 123 끝남
+            // 124 해야지
         }
         // numbers 어레이에 Self-Numbers가 아닌 애들이 다지워진 상황
 
+        //System.out.println("Self-Numbers 들 : ");
         for (int i = 0; i < numbers.size(); i++) {
+            //System.out.printf("%6s", numbers.get(i) + " ");
             total += numbers.get(i);
         }
+        //System.out.println();
         System.out.println("총합: " + total);
     }
 }
