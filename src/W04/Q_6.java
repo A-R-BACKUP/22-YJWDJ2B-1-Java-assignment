5a184f796a5becd499345fd514a4107a738aa5ab
ASTER18K <donhon05@gmail.com>
2022-03-29 15:58:14 +0900

    20220329

---
+++ src/W04/Q_6.java
@@ -0,0 +1,18 @@
+package W04;
+
+import java.util.Scanner;
+
+public class Q_6 {
+    public static void main(String[] args) {
+        String parm =  "";
+        String[] hexa2bin = {
+                "0000", "0001", "0010", "0011",
+                "0100", "0101", "0110", "0111",
+                "1000", "1001", "1010", "1011",
+                "1100", "1101", "1110", "1111"
+        };
+        Scanner sc = new Scanner(System.in);
+        System.out.print("16진수 문자열을 입력하시오: ");
+        
+    }
+}
